9c5cb3f32cf1ec49bd58786a0011d31fe56dad27
saracsilva <saracarolinasilva@gmail.com>
2022-11-02 22:12:26 +0100

    done

--- db/index.js
+++ db/index.js
@@ -5,10 +5,11 @@ const mongoose = require("mongoose");
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/axios-characters-api";
+const MONGO_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/axios-characters-api";
 
 mongoose
-  .connect(MONGO_URI)
+  .connect(MONGO_URI, { family: 4 })
   .then((x) => {
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}"`
--- routes/characters.routes.js
+++ routes/characters.routes.js
@@ -3,25 +3,90 @@ const axios = require("axios");
 
 /* GET home page */
 router.get("/characters", (req, res, next) => {
-    axios.get("https://ih-crud-api.herokuapp.com/characters")
-    .then(responseFromAPI => {
-        // console.log(responseFromAPI)
-        res.render("characters/list-characters", { characters: responseFromAPI.data });
+  axios
+    .get("https://ih-crud-api.herokuapp.com/characters")
+    .then((responseFromAPI) => {
+      // console.log(responseFromAPI)
+      res.render("characters/list-characters", {
+        characters: responseFromAPI.data,
+      });
     })
-    .catch(err => console.error(err))
+    .catch((err) => console.error(err));
 });
-
 
 router.get("/characters/:id", (req, res, next) => {
-    axios.get(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`)
-    .then(responseFromAPI => {
-        // console.log("details: ", responseFromAPI.data)
-        res.render("characters/details-character", { character: responseFromAPI.data });
+  axios
+    .get(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`)
+    .then((responseFromAPI) => {
+      // console.log("details: ", responseFromAPI.data)
+      res.render("characters/details-character", {
+        character: responseFromAPI.data,
+      });
     })
-    .catch(err => console.error(err))
+    .catch((err) => console.error(err));
 });
 
-module.exports = router;
+router.get("/create", (req, res) => {
+  res.render("characters/create-character");
+});
 
+router.post("/create", async (req, res) => {
+  const { name, occupation, weapon, debt } = req.body;
+
+  axios
+    .post("https://ih-crud-api.herokuapp.com/characters", {
+      name,
+      occupation,
+      weapon,
+      debt,
+    })
+    .then((responseFromAPI) => {
+      // console.log("details: ", responseFromAPI.data)
+      res.redirect("/characters");
+    })
+    .catch((err) => console.error(err));
+});
+
+router.get("/characters/:id/edit", (req, res, next) => {
+  axios
+    .get(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`)
+    .then((responseFromAPI) => {
+      // console.log("details: ", responseFromAPI.data)
+      res.render("characters/edit-character", {
+        character: responseFromAPI.data,
+      });
+    })
+    .catch((err) => console.error(err));
+});
+router.post("/characters/:id/update", async (req, res) => {
+  const { name, occupation, weapon, debt } = req.body;
+
+  axios
+    .put(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`, {
+      name,
+      occupation,
+      weapon,
+      debt,
+    })
+
+    .then((responseFromAPI) => {
+      // console.log("details: ", responseFromAPI.data)
+      res.redirect(`/characters/${req.params.id}`);
+    })
+    .catch((err) => console.error(err));
+});
+router.post("/characters/:id/delete", async (req, res) => {
+  const { name, occupation, weapon, debt } = req.body;
+
+  axios
+    .delete(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`)
+
+    .then((responseFromAPI) => {
+      // console.log("details: ", responseFromAPI.data)
+      res.redirect(`/characters`);
+    })
+    .catch((err) => console.error(err));
+});
+module.exports = router;
 
 // https://ih-crud-api.herokuapp.com/characters
